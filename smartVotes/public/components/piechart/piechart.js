d7e0ccb996eb9240feea8e15266b347aac516c87
Nathan Edlen <nathan.edlen@gmail.com>
2017-06-06 08:46:44 -0600

    donor test data found

---
+++ smartVotes/public/components/piechart/piechart.js
@@ -0,0 +1,17 @@
+(function() {
+  'use strict'
+
+  angular.module('app')
+    .component('piechart', {
+      templateUrl: "components/piechart/piechart.html",
+      controller: piechart
+    });
+
+    function piechart($http, showPage, repData) {
+      const vm = this
+
+
+
+    }
+
+})()
